573bfa77a7e9d3e93acbe0e9c596bc68f3b09542
morishun045 <morito.shun8051@mail.kyutech.jp>
2025-11-01 20:23:49 +0900

    modal関連の変更

--- src/app/components/room.tsx
+++ src/app/components/room.tsx
@@ -7,16 +7,13 @@ import styles from "./Room.module.css"
 type Room = {
     roomname: string;
     grade: string;
-    detail: string;
+    subject: string;
 }
 
-export function RoomName({ roomname, grade, detail }: Room) {
+export function RoomName({ roomname, grade, subject }: Room) {
     const [isRoomState, setRoomState] = useState(false);
-    const [showModal, setShowModal] = useState(false);
     let Content;
 
-
-    
     if (isRoomState) {
         Content = (
             <div className={styles.room}>
@@ -29,7 +26,7 @@ export function RoomName({ roomname, grade, detail }: Room) {
                             {grade}
                         </li>
                         <li>
-                            {detail}
+                            {subject}
                         </li>
                     </ul>
                     <button onClick={() => setShowModal(true)} className={styles.baseBtn}>詳細を見る</button>
@@ -50,37 +47,11 @@ export function RoomName({ roomname, grade, detail }: Room) {
         );
     }
 
-    let madal;
-
-    if (showModal) {
-        madal = (
-            <div className={styles.madal}>
-                <div className={styles.madalInner}>
-                    <div className={styles.madalHeader}></div>
-                    <h2 className={styles.roomname}>
-                        {roomname}
-                    </h2>
-                    <div className={styles.content}>
-                        <ul>
-                            <li>
-                                {grade}
-                            </li>
-                            <li>
-                                {detail}
-                            </li>
-                        </ul>
-                        <button onClick={() => setShowModal(false)} className={styles.baseBtn}>戻る</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
 
     return (
         <div>
             {Content}
             {/* <button onClick={()=>setRoomState(true)} className={styles.basebutton}>test</button> */}
-            {madal}
         </div>
     )
 }
